30b55450a24daa7c81e44b318b17763d41002402
Rishabh Kariyana <RK@Rishabhs-MacBook-Pro.local>
2018-01-18 18:11:44 +0530

    Added basictodolist exeercise

---
+++ basictodolist.js
@@ -0,0 +1,37 @@
+var todos = [];
+
+var ans = prompt("What would you like to do?");
+
+while (ans !== "quit") {
+  if (ans === "list") {
+    listTodos();
+  }
+  else if (ans === "new") {
+    addTodo();
+  }
+  else if (ans === "delete") {
+    delTodo();
+  }
+  ans = prompt("What would you like to do?");
+}
+console.log("Ok, you quit the app");
+
+function listTodos() {
+  console.log("***********");
+  todos.forEach(function (todo, i) {
+    console.log(i + ": " + todo);
+  });
+  console.log("***********");
+}
+
+function addTodo() {
+  var newTodo = prompt("Please enter a todo")
+  todos.push(newTodo);
+  console.log("Todo added");
+}
+
+function delTodo() {
+  var delIndex = prompt("Which index of todo to delete?");
+  todos.splice(delIndex, 1);
+  console.log("Todo deleted");
+}
